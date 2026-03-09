eba7aa152f4f8c1c29a7aa51c88220ccaef47981
Jahangir Kazimzada <ckazimzada02@gmail.com>
2026-03-09 17:29:27 +0400

    first

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php 
-    include 'list.php';
+    //include 'list.php';
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="list.php" method="post" name = "form-data">
+        Enter the data: <input type="text" name = "id"><br><br>
+        <button type="submit">Search</button>
+    </form>
+</body>
+</html>
--- list.php
+++ list.php
@@ -1,11 +1,15 @@
 <?php 
     include 'connect_db.php';
 
-    $sql = "SELECT * FROM workers";
-    $list = $conn -> query($sql);
-
-    while($row = $list -> fetch_assoc()){
-        echo("Id: " . $row["id"] . "<br>Name: " . $row["name"] . "<br>Surname: " . $row["surname"] .
-         "<br>Password: " . $row["password"] . "<br>Age: " . $row["age"] . "<br><br>");
+    $searchId = $_POST["id"];
+    if($searchId){
+        $sql = "SELECT * FROM workers WHERE name = '$searchId' or surname = '$searchId' or  age = '$searchId'";
+        $list = $conn -> query($sql);
+        while($row = $list -> fetch_assoc()){
+            echo("Id: " . $row["id"] . "<br>Name: " . $row["name"] . "<br>Surname: " . $row["surname"] .
+            "<br>Password: " . $row["password"] . "<br>Age: " . $row["age"] . "<br><br>");
     }
+    }
+
+    
 ?>
