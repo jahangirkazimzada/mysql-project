ff234235b96390bdf8fe3c86d5d87251cb9b45e0
Jahangir Kazimzada <ckazimzada02@gmail.com>
2026-03-12 10:46:16 +0400

    New

--- index.php
+++ index.php
@@ -4,35 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        require "connect_db.php";
+    <?php
+        include "connect_db.php";
         $sql = "SELECT * FROM workers";
         $list = $conn -> query($sql);
     ?>
     <div class="container">
-        <table class="table">
+        <table>
             <thead>
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Surname</th>
-                    <th scope="col">Password</th>
-                    <th scope="col">Age</th>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Surname</th>
+                    <th>Password</th>
+                    <th>Age</th>
                 </tr>
             </thead>
-            <tbody class="table-group-divider">
-                <!-- <tr>
-                <th scope="row">3</th>
-                    
-                </tr> -->
+            <tbody>
                 <?php while($row = $list -> fetch_assoc()): ?>
-                    <tr>
+                    <tr class = "row">
                         <td><?php echo $row["id"] ?></td>
                         <td><?php echo $row["name"] ?></td>
                         <td><?php echo $row["surname"] ?></td>
@@ -43,5 +37,12 @@
             </tbody>
         </table>
     </div>
+    <script>
+        document.querySelectorAll(".row").forEach((element, index) => {
+            if((index % 2 === 0)){
+                element.style.cssText = "background-color: rgb(138, 167, 167) !important;"
+            }
+        })
+    </script>
 </body>
 </html>
